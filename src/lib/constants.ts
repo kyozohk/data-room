940d0f83b756b964516a86cf6db1e96262fe63b3
aeropriest <ashok.jaiswal@gmail.com>
2026-05-14 16:18:43 +0800

    * Fixed the vertical space

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+/**
+ * Shared site-wide constants. Keep externally-visible URLs here so they
+ * can be updated in one place across CTAs.
+ */
+
+// External DocSend dataroom link wired to every "Enter the Dataroom" CTA.
+export const DATAROOM_URL = 'https://docsend.com/view/s/27n7sgemd82nmc3n';
